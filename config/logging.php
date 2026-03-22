bac687dbec1d1ef5f08112a6f55368189f521302
DenilsonDonr <denilsontrillo@gmail.com>
2026-03-22 10:42:04 +0000

    feat(logging): add admin log channel for separate logging of admin activities

--- config/logging.php
+++ config/logging.php
@@ -127,6 +127,12 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
+        'admin' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/admin.log'),
+            'level' => env('LOG_LEVEL', 'info'),
+            'replace_placeholders' => true,
+        ],
     ],
 
 ];
